3916eaf0d96056acbf7bdff5a6808e2e89a56326
yakattak <stevecalebrese@gmail.com>
2022-05-10 11:31:45 -0400

    resume width update

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import resume from "../../assets/resume/resume.pdf";
+import { useState, useEffect } from 'react';
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+ function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
 
 
 function Resume() {
+    const {height, width} = useWindowDimensions();
+    const useWidth = 0.8*width
+    console.log(useWidth)
     return(
      //   
         <div>
          <a href={resume} target="_blank">Download PDF Resume</a>
-        <embed src={resume} width="100%" height="1000px%"></embed>
+        <embed src={resume} width = {useWidth} height={height}></embed>
         </div>
         
 
