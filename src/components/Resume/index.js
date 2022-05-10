fc8b9eda2a2a73ebec4ca2cb4c1151290142ad70
yakattak <stevecalebrese@gmail.com>
2022-05-09 22:54:24 -0400

    resume download and pdf embed

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import resume from "../../assets/resume/resume.pdf";
+
 
 
 function Resume() {
     return(
-        <p> MY RESUME HERE</p>
+     //   
+        <div>
+         <a href={resume} target="_blank">Download PDF Resume</a>
+        <embed src={resume} width="100%" height="1000px%"></embed>
+        </div>
+        
+
+
+
     )
 }
 
 export default Resume;
+
+
