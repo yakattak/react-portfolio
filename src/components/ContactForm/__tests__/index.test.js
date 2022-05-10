db26ce0e5a0dca90afbe1c607435ec33c4c8a33f
yakattak <stevecalebrese@gmail.com>
2022-05-09 21:44:54 -0400

    using previous project as starter code

---
+++ src/components/ContactForm/__tests__/index.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable jest/no-identical-title */
+// __tests__/ContactForm.test.js
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import ContactForm from '../../ContactForm'
+
+afterEach(cleanup)
+
+describe('ContactForm component renders', () => {
+  it('renders', () => {
+  render(<ContactForm />);
+});
+
+  it('renders', () => {
+    const { asFragment } = render(<ContactForm />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
+
+
+it('renders', () => {
+  const { getByTestId } = render(<ContactForm />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+})
+ 
+it('renders', () => {
+  const { getByTestId } = render(<ContactForm />)
+  expect(getByTestId('button')).toHaveTextContent('Submit')
+})
