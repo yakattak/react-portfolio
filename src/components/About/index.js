3d4c360b3a9f721dc4a32b2235d7e72a56004637
yakattak <stevecalebrese@gmail.com>
2022-09-11 17:07:56 -0400

    updated about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,13 +11,25 @@ function About() {
       </div>
       <div>
         
-        You've done it. You've found the end of the internet and this is your prize...HI! My name is Steve and I like to have a lot irons in the fire. I'm a husband, coach, frisbeer, property manager, teacher, engineer, and (apparently)- developer!
+        Hey there! Thanks for visiting my Portfolio! My name is Steve Calebrese and I am proud to have recently earned my Certificate of Completion from the Columbia Engineering Full Stack Web Development Boot Camp.
 
-I've earned my B.S. and M.S in Materials Science and Engineering from RPI in Troy, NY, as well as my M.A.T in Education from Union Graduate College in Schenectady, NY. From 2015-2021 my full-time job was a physics and engineering teacher at Hoosick Falls High School in Hoosick Falls, NY. As much as I love teaching, I recently decided to take my career in a new direction and enrolled in Full-Stack "bootcamp" through Columbia University. Simulataneously, I have accepted a new position as a Lead Materials Engineer with GE Aviation in Lynn, MA.
-My wife and I are extremely excited for our new adventure and the infinite possibilities my courswork and new career will afford it. Please be sure to check back here frequently for updates!
+My other educational achievements include a B.S. and M.S in Materials Science and Engineering from RPI in Troy, NY, as well as my M.A. in Education from Union Graduate College in Schenectady, NY. From 2015-2021 my full-time job was a physics and engineering teacher at Hoosick Falls High School in Hoosick Falls, NY. 
+Currently I am a Lead Metallic Materials Engineer with GE Aviation in Lynn, MA, however, I am very eager to begin a career in software engineering or back-end web development.
+<br></br>
+<br></br>
+I have always found great joy in finding different ways to solve problems, hence my love for physics and engineering. When I discovered the infinite possibilities for 
+problem solving and finding creative solutions in programming and web development I became immediately drawn in. As I worked on applications, both individually and with collaborators in my boot camp, I frequently
+found myself unable to step away, completely immersed in what I was doing, and enjoying every minute of discovery.
 
-I hope you enjoy!
-      
+<br></br>
+<br></br>
+
+While I am satisfied with my crurent career, I am currently seeking to love what I do. For that reason I am seeking employment in software and/or web development. While 
+I search for a role that speaks to me I am continuing my education by learning AWS, further delving into Python, joining software and web development communities, and looking for others
+to collaborate with on a new project.
+
+
+     
       </div>
     </section>
   )
